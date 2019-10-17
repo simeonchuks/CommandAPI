3a1819a1c98c6ce6a96daef1b1797107b5eeb5c6
Simeon Chuks <simeonchuks20@gmail.com>
2019-10-18 00:59:11 +0100

    Added Migration and Created the Database using EF Core

--- Src/CommandAPI/Controllers/CommandsController.cs
+++ Src/CommandAPI/Controllers/CommandsController.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
+using CommandAPI.Models;
 
 
 namespace CommandAPI.Controllers
@@ -9,10 +10,29 @@ namespace CommandAPI.Controllers
     [ApiController]
     public class CommandsController : ControllerBase
     {
-        [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+
+        private readonly CommandContext _context;
+
+        public CommandsController(CommandContext context)
         {
-            return new string[] { "This is", "my own", "Hard codded", "message" };
+            _context = context;
         }
+
+        //Http Get api/commands
+        [HttpGet]
+        public ActionResult<IEnumerable<Command>> GetCommandItems()
+        {
+            return _context.CommandItems;
+        }
+
+
+
+
+        //THIS METHOD IS USED TO RETURN HARD CODDED DATA
+        // [HttpGet]
+        // public ActionResult<IEnumerable<string>> Get()
+        // {
+        //     return new string[] { "This is", "my own", "Hard codded", "message" };
+        // }
     }
 }
